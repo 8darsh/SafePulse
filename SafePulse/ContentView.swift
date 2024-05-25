e2df0afc5f05bf34c117a1430157d92882691375
Adarsh <adarsh232002@gmail.com>
2024-05-25 14:56:48 +0530

    Initial Commit

---
+++ SafePulse/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SafePulse
+//
+//  Created by Adarsh Singh on 25/05/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SafePulse/SafePulseApp.swift
@@ -0,0 +1,17 @@
+//
+//  SafePulseApp.swift
+//  SafePulse
+//
+//  Created by Adarsh Singh on 25/05/24.
+//
+
+import SwiftUI
+
+@main
+struct SafePulseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
